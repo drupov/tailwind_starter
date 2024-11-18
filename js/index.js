025463ca0dc612b52029e4470ac537b38f223690
Dimitar Rupov <dimitar.rupov@gmail.com>
2024-11-18 18:49:03 +0200

    Add initial js file

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Tailwind starter behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.tailwindStarter = {
+    attach (context, settings) {
+      console.log('Tailwind starter initialized.');
+    }
+  };
+
+} (Drupal));
